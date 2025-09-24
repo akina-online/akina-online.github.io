04da580914a64a4f8ee26780041728cdcd198c4b
akina-online <159863517+akina-online@users.noreply.github.com>
2025-09-24 11:35:35 +0800

    Site updated: 2025-09-24 11:35:35

--- js/fomal.js
+++ js/fomal.js
@@ -3174,10 +3174,10 @@ if (localStorage.getItem("blogbg") != undefined) {
   setBg(localStorage.getItem("blogbg"));
 } else {
   document.getElementById("defineBg").innerText = `:root{
-    --default-bg: url(https://lskypro.acozycotage.net/Fomalhaut/img/dm14.webp);
-    --darkmode-bg:url(https://lskypro.acozycotage.net/Fomalhaut/img/yuanshen1.webp);
-    --mobileday-bg: url(https://lskypro.acozycotage.net/Fomalhaut/img/snow.webp);
-    --mobilenight-bg: url(https://lskypro.acozycotage.net/Fomalhaut/img/mb8.webp);
+    --default-bg: url(https://image-5o7.pages.dev/img/daytime.jpg);
+    --darkmode-bg:url(https://image-5o7.pages.dev/img/night.jpg);
+    --mobileday-bg: url(https://image-5o7.pages.dev/img/daytime-1.jpg);
+    --mobilenight-bg: url(https://image-5o7.pages.dev/img/night-1.jpg);
   }`;
 }
 // 切换背景主函数
